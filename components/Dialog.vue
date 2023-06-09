1221aef64eca0a8d153a0c61c32f71cf657855c1
AaaXya <2939166862@qq.com>
2023-06-09 23:57:16 +0800

    see-you-tomorrow

--- components/Dialog.vue
+++ components/Dialog.vue
@@ -2,17 +2,26 @@
 
   const open = ref(true)
 
-  function shift() {
+  async function shift() {
     console.log(open.value);
 
     open.value = !open.value
 
+    const { data: res, error } = await useFetch('/api/user-id')
+    if (error.value) {
+      console.error(error.value);//'出现了一个预期之外的错误'
+      alert(error.value)
+    }
+
+    //res会返回一个用户id码 
+    //将码存在cookie里
   }
 </script>
 
 <template>
   <div>
     <button @click="shift">
+      我已阅读活动须知 确定
       Open Modal
     </button>
     {{ open }}
